b30fc79f4d7d388cd8d61258633962fccf763a78
jad1staa <jadonami@gmail.com>
2026-09-16 19:32:55 -0400

    class examples black box complete

---
+++ class examples/class examples sep 16/js/script.js
@@ -0,0 +1,28 @@
+/**
+ * Title of Project
+ * Author Name
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+    //single line comment
+    createCanvas(500, 500);
+    background(0,0,0)
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
