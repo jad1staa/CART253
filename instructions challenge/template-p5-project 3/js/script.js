8d1798920e2981b618d5b8003a0ed0caa71e46be
jad1staa <jadonami@gmail.com>
2026-09-16 20:56:07 -0400

    little people and sand instructions challenge

--- instructions challenge/template-p5-project 3/js/script.js
+++ instructions challenge/template-p5-project 3/js/script.js
@@ -21,11 +21,33 @@ function setup() {
 */
 function draw() {
 
-fill(255,0,0);
-stroke(255,255,255)
-strokeWeight(5);
-ellipse(100,100,20,20);
-ellipse(200,100,50,50);
+    createCanvas(500, 500);
+    background(245,235,216);
 
+    fill(255,0,0);
+    stroke(255,255,255);
+    strokeWeight(5);
+    ellipse(100,100,50,50);
+    ellipse(200,100,50,50);
+    ellipse(300,100,50,50);
+    ellipse(400,100,50,50);
+    
+    ellipse(100,70,50,50);
+    ellipse(200,70,50,50);
+    ellipse(300,70,50,50);
+    ellipse(400,70,50,50);
+
+    fill(64,224,208);
+    stroke(255,255,255);
+    strokeWeight(5);
+    ellipse(100,200,50,50);
+    ellipse(200,200,50,50);
+    ellipse(300,200,50,50);
+    ellipse(400,200,50,50);
+
+    ellipse(100,170,50,50);
+    ellipse(200,170,50,50);
+    ellipse(300,170,50,50);
+    ellipse(400,170,50,50);
 
 }
