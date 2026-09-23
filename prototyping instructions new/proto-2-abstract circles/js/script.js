e0eb7dce87bad102f94649fd928848e5fb28a5ef
jad1staa <jadonami@gmail.com>
2026-09-22 21:22:54 -0400

    Update script.js

--- prototyping instructions new/proto-2-abstract circles/js/script.js
+++ prototyping instructions new/proto-2-abstract circles/js/script.js
@@ -15,6 +15,15 @@ function setup() {
     createCanvas(600, 400);
     background(242, 220, 190);
 
+    // Large circles
+    fill(180, 80, 70);
+    ellipse(100, 100, 180);
+
+    fill(210, 110, 80);
+    ellipse(300, 120, 220);
+
+    fill(150, 70, 90);
+    ellipse(500, 100, 160);
     
 }
 
