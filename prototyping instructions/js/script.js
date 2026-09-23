6c2af97edabeca3311ee064c26c2051a77f27485
jad1staa <jadonami@gmail.com>
2026-09-22 20:49:03 -0400

    updated template and title

---
+++ prototyping instructions/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * SUNSET
+ * Jad Nami
+ * 
+ * A simple beach sunset made using basic p5.js drawing instructions.!
+ */
+
+"use strict";
+
+/**
+ * Creates the canvas and draws the sunset scene.
+*/
+function setup() {
+
+}
+
+
+/**
+ * Draws the sunset scene.
+*/
+function draw() {
+
+}
