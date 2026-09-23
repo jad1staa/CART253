3c75d66e6634125a0aefae269184ea834b567316
jad1staa <jadonami@gmail.com>
2026-09-23 18:16:46 -0400

    introducing variables class exercise

---
+++ introducing variables/js/script.js
@@ -0,0 +1,28 @@
+/**
+ * Title of Project
+ * Author Name
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    createCanvas(700, 700);
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+    background(0,0,0);
+    fill(255, 0, 0);
+    ellipse(width/2,height/2,50,50);
+
+}
