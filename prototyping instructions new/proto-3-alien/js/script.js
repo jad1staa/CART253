a2b6a65046426042d356034f3d79a07697a4f3e1
jad1staa <jadonami@gmail.com>
2026-09-22 21:38:31 -0400

    proto 3

---
+++ prototyping instructions new/proto-3-alien/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Alien
+ * Jad Nami
+ *
+ * A simple alien character made using basic p5.js shapes.
+ */
+
+/**
+ * Creates the canvas and draws the alien.
+ */
+function setup() {
+    createCanvas(600, 400);
+    background(30, 35, 55);
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
